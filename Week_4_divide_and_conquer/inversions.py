4cb5cd8951054cb27f69dad3767a2a687142b374
Greg <gregh13@protonmail.com>
2022-09-19 16:14:05 +0700

    remove multiplier variable, fix inversion calculation

--- Week_4_divide_and_conquer/inversions.py
+++ Week_4_divide_and_conquer/inversions.py
@@ -6,7 +6,7 @@ def inversions_better(sequence):
         # Single pass to find all index values of the max value in a list
         if not elements:
             return []
-        max_indices = [0]
+        max_indices = []
         max = elements[0]
         for i, value in enumerate(elements):
             if value < max:
@@ -33,31 +33,30 @@ def inversions_better(sequence):
         if seq_length <= index_length:
             return inversions
 
-        # Multiplier accounts for previous max values that would create inversions in subsequent ranges in sequence
-        multiplier = 1
-
         for index_val in max_index_list:
-            # Remove current iteration from list length
-            index_length -= 1
+            print(f"Max_val: ", max_val)
+            print(f"Max_index_list: ", max_index_list)
+            print(f"Sequence: ", sequence)
+            print(f"Inversions: ", inversions)
 
-            # Minus 1 from seq_len to account for index/len issue, check range from max to end of seq, minus duplicates
-            inversions += (seq_length - 1) - ((index_val - index_length) * multiplier)
+            # Subtract duplicates from sequence length, the get the range from max index to end of sequence
+            inversions += ((seq_length - index_length) - index_val)
 
             # Prepare for next iteration
-            multiplier += 1
+            index_length -= 1
 
             # Remove the current max value from the original sequence, preparing for next iteration with smaller list
             sequence.remove(max_val)
 
 
-
 def inversions_naive(a):
     number_of_inversions = 0
     for i, j in combinations(range(len(a)), 2):
-        print("i: {0}, j: {1}".format(i, j))
-        print("a[i]: {0}, a[j]: {1}".format(a[i], a[j]))
+        # Debug print statements
+        # print("i: {0}, j: {1}".format(i, j))
+        # print("a[i]: {0}, a[j]: {1}".format(a[i], a[j]))
         if a[i] > a[j]:
-            print("Inversion!")
+            # print("Inversion!")
             number_of_inversions += 1
     return number_of_inversions
 
@@ -66,5 +65,5 @@ if __name__ == '__main__':
     input_n = int(input())
     elements = list(map(int, input().split()))
     assert len(elements) == input_n
-    # print(inversions_naive(elements))
+    print(inversions_naive(elements))
     print(inversions_better(elements))
