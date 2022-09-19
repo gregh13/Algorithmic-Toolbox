c2a4a9c8ff73a3566bfc9476faa634d440158ea6
Greg <gregh13@protonmail.com>
2022-09-19 15:24:54 +0700

    create simple pass max indices function

--- Week_4_divide_and_conquer/inversions.py
+++ Week_4_divide_and_conquer/inversions.py
@@ -1,11 +1,30 @@
 from itertools import combinations
 
 
+def inversions_better(sequence):
+    def calc_max_indices(elements):
+        # Single pass to find all index values of the max value in a list
+        if not elements:
+            return []
+        max_indices = [0]
+        max = elements[0]
+        for i, value in enumerate(elements):
+            if value < max:
+                continue
+            elif value == max:
+                max_indices.append(i)
+            else:
+                max = value
+                max_indices = [i]
+        return max_indices
+
+
+
 def inversions_naive(a):
     number_of_inversions = 0
     for i, j in combinations(range(len(a)), 2):
-        print(f"i: {0}, j: {1}".format(i, j))
-        print(f"a[i]: {0}, a[j]: {1}".format(a[i], a[j]))
+        print("i: {0}, j: {1}".format(i, j))
+        print("a[i]: {0}, a[j]: {1}".format(a[i], a[j]))
         if a[i] > a[j]:
             print("Inversion!")
             number_of_inversions += 1
@@ -16,4 +35,5 @@ if __name__ == '__main__':
     input_n = int(input())
     elements = list(map(int, input().split()))
     assert len(elements) == input_n
-    print(inversions_naive(elements))
+    # print(inversions_naive(elements))
+    print(inversions_better(elements))
