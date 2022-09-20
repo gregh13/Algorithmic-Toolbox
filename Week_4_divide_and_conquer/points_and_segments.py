1137c5c01fff6e18d348861a37fcd101c034067e
Greg <gregh13@protonmail.com>
2022-09-20 16:52:11 +0700

    implement slightly better naive algorithm to better understand problem.

--- Week_4_divide_and_conquer/points_and_segments.py
+++ Week_4_divide_and_conquer/points_and_segments.py
@@ -1,6 +1,21 @@
 from sys import stdin
 
 
+def points_naive_better(starts, ends, points):
+    assert len(starts) == len(ends)
+    count = [0] * len(points)
+
+    for i in range(starts):
+        for j in range(points):
+            if starts[i] > points[j]:
+                continue
+            if points[j] < ends[i]:
+                continue
+            if starts[i] <= points[j] <= ends[i]:
+                count[j] += 1
+    return count
+
+
 def points_cover_naive(starts, ends, points):
     assert len(starts) == len(ends)
     count = [0] * len(points)
@@ -19,5 +34,6 @@ if __name__ == '__main__':
     input_starts, input_ends = data[2:2 * n + 2:2], data[3:2 * n + 2:2]
     input_points = data[2 * n + 2:]
 
-    output_count = points_cover_naive(input_starts, input_ends, input_points)
+    # output_count = points_cover_naive(input_starts, input_ends, input_points)
+    output_count = points_naive_better(input_starts, input_ends, input_points)
     print(*output_count)
