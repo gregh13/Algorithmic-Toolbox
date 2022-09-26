c1bcd7c6d6a7814c5a9f45cea0c50f756ec28357
Greg <gregh13@protonmail.com>
2022-09-26 10:30:20 +0700

    add comments

--- Week_5_dynamic_programming_1/change_dp.py
+++ Week_5_dynamic_programming_1/change_dp.py
@@ -9,8 +9,13 @@ def change(money):
     min_num_coins = {0: 0}
 
     for m in range(1, money + 1):
+        # Set number of coins for current money value to positive infinity
         min_num_coins[m] = float("inf")
+
+        # Iterate through all possible coin denominations
         for coin_i in coin_denoms:
+
+            # Check if able to add a coin without going over the money value
             if m >= coin_i:
                 num_coins = min_num_coins[m - coin_i] + 1
                 if num_coins < min_num_coins[m]:
