00de98e64f827a9fe9a30bdcb7881c2c91e9f942
Greg <gregh13@protonmail.com>
2022-09-26 15:53:22 +0700

    add debug print statements

--- Week_5_dynamic_programming_1/edit_distance.py
+++ Week_5_dynamic_programming_1/edit_distance.py
@@ -5,8 +5,21 @@ def edit_distance(first_string, second_string):
     # Create 2d matrix
     string_matrix = [[0 for x in range(second_length)] for y in range(first_length)]
 
+    for j in range(1, second_length):
+        for i in range(1, first_length):
+            insertion = string_matrix[i][j-1] + 1
+            deletion = string_matrix[i-1][j] + 1
+            match = string_matrix[i-1][j-1]
+            mismatch = string_matrix[i-1][j-1] + 1
 
-    return 0
+            print("Matrix before: ", string_matrix)
+            if first_string[i] == second_string[j]:
+                string_matrix[i][j] = min(insertion, deletion, match)
+            else:
+                string_matrix[i][j] = min(insertion, deletion, mismatch)
+            print("Matrix after: ", string_matrix)
+
+    return string_matrix[first_length-1][second_length-1]
 
 
 if __name__ == "__main__":
