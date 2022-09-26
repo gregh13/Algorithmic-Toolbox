d721feafcba8a6c7491d0542b8a7e5ad79bccb69
Greg <gregh13@protonmail.com>
2022-09-26 18:31:25 +0700

    create initial nested loop and sequence length counter

--- Week_5_dynamic_programming_1/lcs2.py
+++ Week_5_dynamic_programming_1/lcs2.py
@@ -1,12 +1,18 @@
 def lcs2(first_sequence, second_sequence):
 
-    longest_seq = 0
+    first_length = len(first_sequence) + 1
+    second_length = len(second_sequence) + 1
 
     # Create 2d matrix with first seq numbers as the columns and second seq numbers as the rows
     # Initialize all values to 0 as this will be our 'memory' array
     seq_2d_array = [[0 for x in range(len(first_sequence))] for y in range(len(second_sequence))]
 
-
+    for i in range(first_length):
+        points_to_add = 0
+        for j in range(second_length):
+            if first_sequence[i] == second_sequence[j]:
+                points_to_add = 1
+            seq_2d_array[i][j] = seq_2d_array[i][j-1] + points_to_add
 
     return 0
 
