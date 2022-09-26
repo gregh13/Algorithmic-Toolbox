7209831d2e6423609173f384ae61e1c7360224ca
Greg <gregh13@protonmail.com>
2022-09-26 18:18:36 +0700

    update comments

--- Week_5_dynamic_programming_1/edit_distance.py
+++ Week_5_dynamic_programming_1/edit_distance.py
@@ -1,15 +1,10 @@
 def edit_distance(first_string, second_string):
-
-    # Add dummy initial variable to help string index and loop index match
-    # first_string = "0" + first_string
-    # second_string = "0" + second_string
-
+    # Add one to length to account for dynamic looping below
     first_length = len(first_string) + 1
     second_length = len(second_string) + 1
 
     # Create 2d matrix with first string chars as the rows and first second string char as the columns
     string_matrix = [[0 for x in range(second_length)] for y in range(first_length)]
-
 
     for j in range(1, second_length):
         # Initialize the starting number of actions taken to get to column j
@@ -26,6 +21,7 @@ def edit_distance(first_string, second_string):
             mismatch = string_matrix[i-1][j-1] + 1
 
             # Save number of actions for next step (options depend on if letters are a match or mismatch)
+            # Subtract one from i and j that we added at beginning (otherwise index error on string occurs)
             if first_string[i-1] == second_string[j-1]:
                 string_matrix[i][j] = min(insertion, deletion, match)
             else:
