beac31b252ffa3bf31a069a796406c33f4da7757
Greg <gregh13@protonmail.com>
2022-09-26 18:48:19 +0700

    fix matrix config and loop index variables

--- Week_5_dynamic_programming_1/lcs2.py
+++ Week_5_dynamic_programming_1/lcs2.py
@@ -5,16 +5,16 @@ def lcs2(first_sequence, second_sequence):
 
     # Create 2d matrix with first seq numbers as the columns and second seq numbers as the rows
     # Initialize all values to 0 as this will be our 'memory' array
-    seq_2d_array = [[0 for x in range(len(first_sequence))] for y in range(len(second_sequence))]
+    seq_2d_array = [[0 for x in range(len(second_sequence))] for y in range(len(first_sequence))]
 
-    for i in range(first_length):
+    for j in range(1, second_length):
         points_to_add = 0
-        for j in range(second_length):
-            if first_sequence[i] == second_sequence[j]:
+        for i in range(1, first_length):
+            if first_sequence[i-1] == second_sequence[j-1]:
                 points_to_add = 1
-            seq_2d_array[i][j] = seq_2d_array[i][j-1] + points_to_add
+            seq_2d_array[i][j] = seq_2d_array[i-1][j] + points_to_add
 
-    return 0
+    return seq_2d_array[first_length-1][second_length-1]
 
 
 if __name__ == '__main__':
