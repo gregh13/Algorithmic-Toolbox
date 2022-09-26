d3996e3d336e2a2a276cce27711081901c0a7363
Greg <gregh13@protonmail.com>
2022-09-26 18:22:32 +0700

    begin dynamic programming implementation for longest common subsequence in two sequences

--- Week_5_dynamic_programming_1/lcs2.py
+++ Week_5_dynamic_programming_1/lcs2.py
@@ -1,4 +1,13 @@
 def lcs2(first_sequence, second_sequence):
+
+    longest_seq = 0
+
+    # Create 2d matrix with first seq numbers as the columns and second seq numbers as the rows
+    # Initialize all values to 0 as this will be our 'memory' array
+    seq_2d_array = [[0 for x in range(len(first_sequence))] for y in range(len(second_sequence))]
+
+
+
     return 0
 
 
