097d057ae7e817a4b8a42da00c9d771c50034bfd
Greg <gregh13@protonmail.com>
2022-09-26 15:29:50 +0700

    begin dynamic programming implementation for edit_distance

--- Week_5_dynamic_programming_1/edit_distance.py
+++ Week_5_dynamic_programming_1/edit_distance.py
@@ -1,4 +1,11 @@
 def edit_distance(first_string, second_string):
+    first_length = len(first_string)
+    second_length = len(second_string)
+
+    # Create 2d matrix
+    string_matrix = [[0 for x in range(second_length)] for y in range(first_length)]
+
+
     return 0
 
 
