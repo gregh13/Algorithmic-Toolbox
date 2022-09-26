4d096ecc81471732c20ca60e788ef767520d3b70
Greg <gregh13@protonmail.com>
2022-09-26 13:56:44 +0700

    change loop range from starting at 1 to starting at 2 (since 1 is already initialized into dict)

--- Week_5_dynamic_programming_1/primitive_calculator.py
+++ Week_5_dynamic_programming_1/primitive_calculator.py
@@ -19,7 +19,7 @@ def compute_operations(n):
 
     num_ops_dict = {1: (1, [1])}
 
-    for number in range(1, n + 1):
+    for number in range(2, n + 1):
         # Initialize output list
         output_seq = [1]
 
