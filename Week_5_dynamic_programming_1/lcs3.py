11c195224f0a5480af7b5bf3555c7010955ed66b
Greg <gregh13@protonmail.com>
2022-09-27 16:16:35 +0700

    add sequence counter logic, checking that match indices are increasing and sequence count is optimal

--- Week_5_dynamic_programming_1/lcs3.py
+++ Week_5_dynamic_programming_1/lcs3.py
@@ -16,6 +16,17 @@ def lcs3(first_sequence, second_sequence, third_sequence):
             if third_sequence[k] == match_list[j][0]:
                 match_list[j].append(k)
 
+    match_list_length = len(match_list)
+    sequence_count = [1] * match_list_length
+
+    for i in range(match_list_length):
+        for j in range(i):
+
+            if match_list[j][1] < match_list[i][1] and sequence_count[i] < (sequence_count[j] + 1) \
+                    and match_list[j][2] < match_list[i][2] and match_list[j][3] < match_list[i][3]:
+                # Update sequence count to one more than the best recent count
+                sequence_count[i] = sequence_count[j] + 1
+
     return 0
 
 if __name__ == '__main__':
