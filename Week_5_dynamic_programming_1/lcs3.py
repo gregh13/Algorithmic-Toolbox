1950da163ce70ba2b43701847e1817ed3c6d4fe9
Greg <gregh13@protonmail.com>
2022-09-27 16:04:40 +0700

    begin implementation of dynamic programming for finding the longest common subsequences in 3 sequences

--- Week_5_dynamic_programming_1/lcs3.py
+++ Week_5_dynamic_programming_1/lcs3.py
@@ -1,4 +1,17 @@
 def lcs3(first_sequence, second_sequence, third_sequence):
+    # Make set for faster verification in steps below
+    third_set = set(third_sequence)
+
+    match_list = []
+
+    # Compared sequences item by item, add any matches to list
+    for i in range(len(first_sequence)):
+        for j in range(len(second_sequence)):
+            if first_sequence[i] == second_sequence[j] and first_sequence[i] in third_set:
+                # Adds index of item in each sequence, used to eliminate duplicates in next step
+                match_list.append([first_sequence[i], j, i])
+
+
     return 0
 
 if __name__ == '__main__':
