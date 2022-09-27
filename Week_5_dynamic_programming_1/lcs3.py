c105be1884c2a1f61f5701a54bc6ebc16ef136ca
Greg <gregh13@protonmail.com>
2022-09-27 18:54:12 +0700

    add comments

--- Week_5_dynamic_programming_1/lcs3.py
+++ Week_5_dynamic_programming_1/lcs3.py
@@ -1,11 +1,12 @@
 def lcs3(first_sequence, second_sequence, third_sequence):
 
+    # Filter out non-relevant items in the lists, leaves only numbers shared by all three
     first_sequence = [x for x in first_sequence if x in set(second_sequence) and x in set(third_sequence)]
     second_sequence = [y for y in second_sequence if y in first_sequence]
     third_sequence = [z for z in third_sequence if z in first_sequence]
     match_list = []
 
-    # Compared sequences item by item, add any matches to list
+    # Compared sequences item by item, add any matches to list as a tuple with the indices for each list in order
     for i in range(len(first_sequence)):
         first = first_sequence[i]
         for j in range(len(second_sequence)):
@@ -14,16 +15,22 @@ def lcs3(first_sequence, second_sequence, third_sequence):
                     if first == third_sequence[k]:
                         match_list.append((i, j, k))
 
+    # Initialize sequence count array to length of match_list
     match_list_length = len(match_list)
     sequence_count = [1] * match_list_length
 
     for i in range(match_list_length):
+        # Grab value as is used multiple times below
         i_match = match_list[i]
+
         for j in range(i):
+            # Grab value as is used multiple times below
             j_match = match_list[j]
 
+            # Check if sequence count will be optimal, then checks if index increases in each sequences
             if sequence_count[i] < (sequence_count[j] + 1) and j_match[0] < i_match[0]\
                     and j_match[1] < i_match[1] and j_match[2] < i_match[2]:
+
                 # Update sequence count to one more than the best recent count
                 sequence_count[i] = sequence_count[j] + 1
 
