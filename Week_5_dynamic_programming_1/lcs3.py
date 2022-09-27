c76556ccb5c795ae9aac033b0f84d8d1b91e9797
Greg <gregh13@protonmail.com>
2022-09-27 18:46:48 +0700

    add set function in list comprehension

--- Week_5_dynamic_programming_1/lcs3.py
+++ Week_5_dynamic_programming_1/lcs3.py
@@ -1,6 +1,6 @@
 def lcs3(first_sequence, second_sequence, third_sequence):
 
-    first_sequence = [x for x in first_sequence if x in second_sequence and x in third_sequence]
+    first_sequence = [x for x in first_sequence if x in set(second_sequence) and x in set(third_sequence)]
     second_sequence = [y for y in second_sequence if y in first_sequence]
     third_sequence = [z for z in third_sequence if z in first_sequence]
     match_list = []
@@ -12,7 +12,7 @@ def lcs3(first_sequence, second_sequence, third_sequence):
             if first == second_sequence[j]:
                 for k in range(len(third_sequence)):
                     if first == third_sequence[k]:
-                        match_list.append((j, i, k))
+                        match_list.append((i, j, k))
 
     match_list_length = len(match_list)
     sequence_count = [1] * match_list_length
