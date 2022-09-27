0e10b86f4748cdb754cacede88cacf23535db889
Greg <gregh13@protonmail.com>
2022-09-27 10:09:12 +0700

    add sequence count loop logic

--- Week_5_dynamic_programming_1/lcs2.py
+++ Week_5_dynamic_programming_1/lcs2.py
@@ -3,12 +3,21 @@ def lcs2(first_sequence, second_sequence):
     first_length = len(first_sequence)
     second_length = len(second_sequence)
 
-    sequence_count = []
-    index_match = []
+    index_match_list = []
     for i in range(first_length):
         for j in range(second_length):
             if first_sequence[i] == second_sequence[j]:
-                index_match.append(j)
+                index_match_list.append(j)
+
+    length_of_index_matches = len(index_match_list)
+    sequence_count = [1] * length_of_index_matches
+
+    for i in range(length_of_index_matches):
+        for j in range(i):
+            if index_match_list[j] < index_match_list[i] and sequence_count[i] < (sequence_count[j] + 1):
+                sequence_count[i] = sequence_count[j] + 1
+
+
 
 
 
