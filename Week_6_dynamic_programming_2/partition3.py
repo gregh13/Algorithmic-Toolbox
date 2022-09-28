1b887454c0d0e85a50c50f98c72868f6452a51d6
Greg <gregh13@protonmail.com>
2022-09-28 15:13:23 +0700

    begin implementing partition3 algorithm

--- Week_6_dynamic_programming_2/partition3.py
+++ Week_6_dynamic_programming_2/partition3.py
@@ -2,10 +2,20 @@ from sys import stdin
 
 
 def partition3(values):
-    return 0
+    can_split_evenly = False
+
+    ways_to_split = 3
+    val_sum = sum(values)
+    if val_sum % ways_to_split != 0:
+        return can_split_evenly
+
+    share_value = val_sum / ways_to_split
+
+    return can_split_evenly
 
 
 if __name__ == '__main__':
-    input_n, *input_values = list(map(int, stdin.read().split()))
+    # input_n, *input_values = list(map(int, stdin.read().split()))
+    input_n, *input_values = list(map(int, input().split()))
     assert input_n == len(input_values)
     print(partition3(input_values))
