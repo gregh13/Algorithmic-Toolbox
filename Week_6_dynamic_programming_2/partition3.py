f1b87ea045d7e0d3be2d3bd073271885735fc890
Greg <gregh13@protonmail.com>
2022-09-28 17:00:49 +0700

    add statement to catch when an item is larger than share_value (meaning impossible to split)

--- Week_6_dynamic_programming_2/partition3.py
+++ Week_6_dynamic_programming_2/partition3.py
@@ -32,6 +32,9 @@ def partition3(values):
     share_value = val_sum / ways_to_split
 
     values.sort(reverse=True)
+
+    if values[0] > share_value:
+        return int(can_split_evenly)
     memoization_dict = {0: True}
     for n in range(ways_to_split):
         can_split_evenly = find_split(values, share_value)
