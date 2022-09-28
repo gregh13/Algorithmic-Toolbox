19fa1002e30968f79bf263799b4ccb2f9d958b73
Greg <gregh13@protonmail.com>
2022-09-28 16:04:43 +0700

    create memoization recursive function to calc one set of numbers that equals the share value (sum of list / shares)

--- Week_6_dynamic_programming_2/partition3.py
+++ Week_6_dynamic_programming_2/partition3.py
@@ -2,6 +2,18 @@ from sys import stdin
 
 
 def partition3(values):
+    def find_split(value_list, remaining_sum):
+        if remaining_sum in memoization_dict:
+            return memoization_dict[remaining_sum]
+
+        val = 0
+        for i in range(len(value_list)):
+            i_value = value_list[i]
+            if i_value <= remaining_sum:
+                sequence = find_split(value_list[1:], remaining_sum - i_value) + i_value
+
+
+
     can_split_evenly = False
 
     ways_to_split = 3
@@ -9,7 +21,17 @@ def partition3(values):
     if val_sum % ways_to_split != 0:
         return can_split_evenly
 
+    if 0 < len(values) < ways_to_split:
+        return can_split_evenly
+
     share_value = val_sum / ways_to_split
+
+    values.sort()
+    memoization_dict = {0: 0}
+    for n in range(ways_to_split):
+        find_split(values, share_value)
+
+
 
     return can_split_evenly
 
