3c5cd419251b66a4bf17c771f5e1bbf2b734122f
Greg <gregh13@protonmail.com>
2022-09-29 12:49:38 +0700

    reformat input data to more useful output (a list of the number and a list of the operators)

--- Week_6_dynamic_programming_2/placing_parentheses.py
+++ Week_6_dynamic_programming_2/placing_parentheses.py
@@ -10,9 +10,17 @@ def evaluate(a, b, op):
 
 
 def maximum_value(dataset):
-    # write your code here
+
     return 0
 
 
 if __name__ == "__main__":
+    input_data = list(input().split())
+    length = len(input_data)
+    n_raw = (length - 1) / 2
+    n = int(n_raw)
+    assert float(n) == n_raw
+
+
+
     print(maximum_value(input()))
