52236228bdd645ce2190bcd74a14eea7d38b6979
Greg <gregh13@protonmail.com>
2022-09-29 12:20:45 +0700

    add comments

--- Week_6_dynamic_programming_2/knapsack.py
+++ Week_6_dynamic_programming_2/knapsack.py
@@ -6,19 +6,34 @@ def maximum_gold(capacity, weights):
         if max_cap in memoization_dict:
             return memoization_dict[max_cap]
 
+        # Initialize value
         max_cap_val = 0
+
+        # Loop through all possible weight values
         for i in range(len(weight_list)):
             weight_i = weight_list[i]
+
+            # Check if object is too heavy to pick
             if weight_i <= max_cap:
-                val = choose_bar(max_cap - weight_i, (weight_list[:i] + weight_list[i+1:])) + 1
+                # Find next optimal choice if weight_i chosen (removed from weight list as well)
+                val = choose_bar(max_cap - weight_i, (weight_list[:i] + weight_list[i+1:])) + weight_i
                 if val > max_cap_val:
+                    # Save more optimal value
                     max_cap_val = val
+
+        # Update dictionary to value (either 0 or val from loop)
         memoization_dict[max_cap] = max_cap_val
         return memoization_dict[max_cap]
 
+    # Sort weights into increasing order
     weights.sort()
+
+    # Initialize dictionary
     memoization_dict = {0: 0}
+
+    # Start recursive memoization call
     choose_bar(capacity, weights)
+
     return memoization_dict[capacity]
 
 
